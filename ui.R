21196450968de32f619f36f8a18cd53140c93032
onkar964 <62983112+onkar964@users.noreply.github.com>
2020-05-21 09:33:37 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,33 @@
+## Load Shiny library
+library(shiny)
+
+# We will analyse Water Quality Data Set taken from https://data.gov.in and will show state-wise analysis on some parameters
+# First make UI for the application 
+
+shinyUI(fluidPage(
+     # Application title
+     titlePanel("Analyzing Water Quality levels of River Ganga (India) in various states on-route (Year 2012)"),
+     br(),
+     br(),
+     
+     
+     sidebarPanel(
+          helpText("Analyse Water Quality of River Ganga (State wise)"),
+          selectInput("var", 
+                      label = "Choose an state",
+                      choices = c("Maharashtra", "UTTAR PRADESH",
+                                  "BIHAR", "WEST BENGAL"),
+                      selected = "UTTARANCHAL"),
+          helpText("On based of the state selected we'll populate a table on right hand side with crucial parameters of the water of Ganga River and below the data table we'll make a plot of Coliform bacteria in different states on based of water sample")),
+     br(),
+     br(),
+     # Call Data table    
+     mainPanel(dataTableOutput('Details'),
+               # Pass state name
+               textOutput("text1"),
+               br(),
+               br(),
+               # Plot coliform levels state-wise
+               plotOutput("plot1"))
+)
+)
